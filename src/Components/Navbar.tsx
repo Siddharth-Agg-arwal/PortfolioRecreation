4d1c55c206f4e1666e1c20b7591fd769a3c8ad06
Siddharth Aggarwal <saggarwal_be22@thapar.edu>
2023-12-20 17:26:55 +0530

    added css to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,24 @@
 import { NavLink } from "react-router-dom";
+// import { Open_Sans } from 'next/font/google';
+
+// const openSans = Open_Sans({
+//     style:normal,
+// });
 
 export function NavBar() {
   return (
+    <div>
     <nav className="navbar-container">
-      <NavLink to="/">HI! I'M PRABHAV KHANDELWAL.</NavLink>
-      <NavLink to="/work">DESIGN & CODE,</NavLink>
-      <NavLink to="/blog">WORDS & DOODLES,</NavLink>
-      <NavLink to="/about">BIO & FACE</NavLink>
-      
+      <div className="leftNav">
+        <NavLink to="/">HI! I'M PRABHAV KHANDELWAL.</NavLink>
+      </div>
+      <div className="rightNav">
+        <NavLink to="/work">DESIGN & CODE,</NavLink>
+        <NavLink to="/blog">WORDS & DOODLES,</NavLink>
+        <NavLink to="/about">BIO & FACE</NavLink>
+      </div>
     </nav>
+    <div className="navHorizontalRule"><hr /></div>
+    </div>
   );
 }
