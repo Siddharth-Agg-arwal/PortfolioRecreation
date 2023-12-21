2e04718757377600eea9f62c9eebc558a875d61d
Siddharth Aggarwal <saggarwal_be22@thapar.edu>
2023-12-21 15:27:09 +0530

    blog page footer and about page

--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from './CSS/AboutPage.module.css';
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className={styles.aboutpage}>
+      <div className={styles.rightContent}>
+        
+      </div>
+      <div className={styles.leftContent}>
+
+      </div>
+
+      {/* Footer */}
+      <div className={styles.blogpage_footer}>
+        <div className="navHorizontalRule"><hr /></div>
+          <div className={styles.footerContent}>
+            <p className={styles.leftFooter}>THE WEB SURE IS FLUID & DYNAMIC, BUT THAT ALSO MAKES THE THINGS IT HOLDS EPHEMERAL.</p>
+            <p className={styles.rightFooter}>THANKS FOR STOPPING BY!</p>
+         </div>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/BlogPage.tsx
+++ src/Pages/BlogPage.tsx
@@ -65,6 +65,13 @@ const BlogPage = () => {
                 />
             </div>
         </div>
+        <div className={styles.blogpage_footer}>
+        <div className="navHorizontalRule"><hr /></div>
+        <div className={styles.footerContent}>
+          <p className={styles.leftFooter}>THE WEB SURE IS FLUID & DYNAMIC, BUT THAT ALSO MAKES THE THINGS IT HOLDS EPHEMERAL.</p>
+          <p className={styles.rightFooter}>THANKS FOR STOPPING BY!</p>
+        </div>
+      </div>
     </div>
   )
 }
